ce2a82ef8ea8aa1642646867b3d9a183818c329e
Matheus <maathe.f@gmail.com>
2020-04-08 03:47:07 -0400

    Novo layout

---
+++ old/js/app.js
@@ -0,0 +1,26 @@
+/* ****************************************************** */
+/*
+/* Angular app comunication aplication.
+/* 
+/* Var @var myVariableApp "Recebe angularModule"	
+/* Desenvolvido por Matheus Ferreira <maathe.f@gmail.com>
+/*
+/* ****************************************************** */
+var moduleApp = angular.module('moduleApp', [ 'ui.router'])
+
+
+moduleApp.run(["$state",function($state){$state.go('index');}]);
+
+// var firebaseConfig = {
+//     apiKey: "AIzaSyAewRtwoFBVo1dKW2Cjk4ZjcolWjldDLwI",
+//     authDomain: "synchrodelic.firebaseapp.com",
+//     databaseURL: "https://synchrodelic.firebaseio.com",
+//     projectId: "synchrodelic",
+//     storageBucket: "synchrodelic.appspot.com",
+//     messagingSenderId: "876948348825",
+//     appId: "1:876948348825:web:769fae88e4602371"
+//   };
+  
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+  
